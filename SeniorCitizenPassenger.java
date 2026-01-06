15cbc18e71f90c8be125dd535abea8001bfae876
Harika-i <harika1996h@gmail.com>
2026-01-06 22:21:24 +0530

    Add @Override annotation to calculateFare method

--- SeniorCitizenPassenger.java
+++ SeniorCitizenPassenger.java
@@ -5,9 +5,11 @@ public class SeniorCitizenPassenger extends Passenger{
 	public SeniorCitizenPassenger(int id, String name, int age) {
 		super(id, name, age);
 	}
+	@Override
 	public double calculateFare(double baseFare) {
 		return baseFare*0.7; // 30% discount
 	}
 
 
 }
+
