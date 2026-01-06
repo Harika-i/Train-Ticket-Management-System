516402250875705e81a43eec4813009162f96816
Harika-i <harika1996h@gmail.com>
2026-01-06 22:19:52 +0530

    Update ChildPassenger.java

--- ChildPassenger.java
+++ ChildPassenger.java
@@ -4,10 +4,11 @@ public class ChildPassenger extends Passenger{
 	public ChildPassenger(int  id,String name,int age) {
 		super(id, name, age);
 	}
-	
+	@Override
 	public double calculateFare(double baseFare) {
 		return baseFare*0.5; 
 	}
 
 
 }
+
